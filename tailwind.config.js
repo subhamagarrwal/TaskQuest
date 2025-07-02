4a6ef0c74bc40a29a6bd03558369129ea4bee576
subhamagarrwal <130570909+subhamagarrwal@users.noreply.github.com>
2025-07-03 01:47:39 +0530

    dev dependency for build

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./views/**/*.ejs",
+    "./public/**/*.{js,html}",
+    "./src/**/*.{js,html}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
